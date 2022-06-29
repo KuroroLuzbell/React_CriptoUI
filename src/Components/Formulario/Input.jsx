a33c272db61d5921a52d35eba69a8c10abe4d6fb
fernandovalenzuela <fernandovalenzuelaibarra@gmail.com>
2022-06-28 21:42:58 -0400

    Merge branch 'Master' of https://github.com/KuroroLuzbell/React_CriptoUI into Master

--- src/Components/Formulario/Input.jsx
+++ src/Components/Formulario/Input.jsx
@@ -2,15 +2,22 @@ import {Label,GrupoInput,LeyendaError,IconoValidacion, Input} from '../../Elemen
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
-const ComponenteInput = () => {
+const ComponenteInput = ({label,placeHolder,tipo,name,leyendaError,expresionRegular}) => {
   return (
     <div>
-        <Label htmlFor='Email'>Email</Label>
+        <Label htmlFor='Email'>{label}</Label>
         <GrupoInput>                
-            <Input className="form-control form-control-lg" type="email" name="Email" id='Email' placeholder="email"/>
+            <Input 
+              className="form-control form-control-lg" 
+              type={tipo} 
+              name={name} 
+              id={name} 
+              placeholder={placeHolder}
+              
+              />
             <IconoValidacion icon={faCheckCircle}/>
         </GrupoInput>
-        <LeyendaError>lorem ipsun dolor sit</LeyendaError>
+        <LeyendaError>{leyendaError}</LeyendaError>
     </div>
   )
 }
--- src/Components/Usuarios/IniciarSesion.jsx
+++ src/Components/Usuarios/IniciarSesion.jsx
@@ -1,9 +1,7 @@
 
 import { useForm } from '../../hooks/useForm';
 import UsuarioService from '../../Services/UsuarioService';
-import {Formulario,FormIniciarSesion, Label,GrupoDiv,Input} from '../../Elements/Formulario';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import {Formulario, Label,GrupoInput,Input} from '../../Elements/Formulario';
 import ComponenteInput from '../Formulario/Input'
 
 
@@ -42,7 +40,22 @@ export const IniciarSesion = () => {
         <div className='col-3'></div>
         <div className='col-6 offset-lg-3 '>
         <Formulario className=''>
-            <ComponenteInput/>
+            <ComponenteInput
+                label="usuario"
+                placeHolder="john123"
+                tipo="text"
+                name="Email"
+                leyendaError="texto"
+                expresionRegular=""
+            />
+             <ComponenteInput
+                label="clave"
+                placeHolder="****"
+                tipo="password"
+                name="Texto"
+                leyendaError="texto"
+                expresionRegular=""
+            />
             <div className="text-center mt-3">
                 <button type="submit" className="btn btn-lg btn-primary">Iniciar Sesión</button>                                                
             </div>
